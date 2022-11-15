a4b7c40641a043c13bb97ecfb9be1120f945ee32
fateh.aliverdievv <tu7l7qsfn@code.edu.az>
2022-11-15 22:46:47 +0400

    16 november task done

---
+++ 16novembertask/Program.cs
@@ -0,0 +1,50 @@
+﻿#region Ededin reqemlerinin en boyuyu
+//int number = Convert.ToInt32(Console.ReadLine());
+//int lastdigit = 0;
+//int max = 0;
+//if(number < 0)
+//{
+//    number *= -1;
+//}
+//while (number > 0)
+//{
+//    lastdigit = number % 10;
+//    if (lastdigit > max)
+//    {
+//        max = lastdigit;
+//    }
+//    number = number / 10;
+//}
+//Console.WriteLine(max);
+#endregion
+
+#region ay ve gune gore reserve olunma
+//Console.Write("Enter the month:");
+//string month = Console.ReadLine();
+//Console.Write("Enter the day : ");
+//int day = Convert.ToInt32(Console.ReadLine());
+//do
+//{
+//    if ((month == "February") &&(day > 0 && day <= 29))
+//    {
+//        Console.WriteLine(month + " " + " " + day + " " + "successfully reserved");
+//    }
+//    else if ((month == "January" || month == "March" || month == "May" || month == "July" || month == "August" || month == "October" || month == "December") && (day > 0 && day <= 31))
+//    {
+//        Console.WriteLine(month + " " + " " + day + " " + "successfully reserved");
+//    }
+//    else if ((month == "April" || month == "June" || month == "September" || month == "November") && (day > 0 && day <= 30))
+//    {
+//        Console.WriteLine(month + " " + " " + day + " " + "successfully reserved");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Wrong request try again!");
+//        Console.Write("Enter the month:");
+//        month = Console.ReadLine();
+//        Console.Write("Enter the day : ");
+//        day = Convert.ToInt32(Console.ReadLine());
+//    }
+
+//} while (month != "January" && month != "February" && month != "March" && month != "April" && month!="May" && month != "June" && month != "July" && month != "August" && month != "September" && month != "October" && month != "November" && month != "December");
+#endregion
